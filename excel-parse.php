1fad6b155a0dda104430fcd7602fe9019f86f2fa
liruqi <liruqi@gmail.com>
2015-11-02 09:35:25 +0000

    Add google mirror & excel parsing api

---
+++ excel-parse.php
@@ -0,0 +1,70 @@
+<?php
+
+if (isset($_SERVER["REMOTE_ADDR"])) {
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<title>api.liruqi.info</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<?php
+
+echo $_SERVER["REMOTE_ADDR"] . "<br/>";
+}
+
+require_once 'vendor/PHPExcel-1.8.1/Classes/PHPExcel.php';
+
+        $root =  dirname(__FILE__);
+        $name = "test-25.xls";
+        $importFile = $root . "/uploads/" . $name;
+        $objDateTime = new DateTime('NOW'); 
+        
+        $yes = array(); $no = array(); $cnt=0; $ignore = array();
+
+        $nsImportFields = array('id', 'title', 'C', 'D', 'brand');
+
+        $objPHPExcel =  PHPExcel_IOFactory::load($importFile);
+        //$objPHPExcel->setReadDataOnly(TRUE);
+        $objWorksheet = $objPHPExcel->getActiveSheet();
+        echo 'Import file: ' . $importFile . PHP_EOL;
+        foreach ($objWorksheet->getRowIterator() as $row) {
+            $cnt += 1;
+            if ($cnt <=1 ) continue; // skip table head
+
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(FALSE); 
+            $nsUser = array();
+            
+            $index = 0;
+            foreach ($cellIterator as $cell) {
+                $val = "";
+                if (array_key_exists($index, $nsImportFields)) {
+                    $val = $cell->getValue();
+                    
+                    $nsUser[$nsImportFields[$index]] = $val;
+                    $index += 1;
+                } 
+            }
+            
+            if (empty ($nsUser['id'])) break;
+            
+            echo $nsUser['id'] . PHP_EOL;
+            if (strpos($nsUser['title'], $nsUser['brand'])===0) {
+                $yes [] = $nsUser;
+                $suffix = substr($nsUser['title'], strlen($nsUser['brand']));
+                $objPHPExcel->getActiveSheet()->setCellValue('F' . $cnt, $suffix);
+            echo json_encode($nsUser) . PHP_EOL;
+            } else {
+                echo ($nsUser['title'] . " " .  $nsUser['brand']. PHP_EOL);
+            }
+        }
+        echo "<p>导入成功" . count($yes) . ", 失败" . count($no) . ", 忽略(无变更)" . count($ignore) . "</p>";
+        echo "<p>导入成功: " . implode(', ', $yes) . "</p>";
+        echo "<p>导入失败: " . implode(', ', $no) . "</p>";
+        echo "<p>无变更: " . implode(', ', $ignore) . "</p>";
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save($root . "/uploads/" . time() . $name);
+
