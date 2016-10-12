d34d1a9c64461d45a86a7c28d0342fd0b9db4db0
liruqi <liruqi@gmail.com>
2016-10-12 14:07:58 +0800

    Fix description string

--- mume-rulesets.php
+++ mume-rulesets.php
@@ -1,9 +1,13 @@
 <?php
+$names = array();
 $rulesetdir =  dirname(__FILE__) . "/ruleset";
 $files = array_diff(scandir($rulesetdir), ['.', '..']);
 
 $jsonArray = [];
 foreach($files as $key => $name) {
+    $uuid = explode(".", $name)[0];
+    if (isset($names[$uuid])) continue;
+    $names[$uuid] = 1;
     if (explode(".", $name)[1] == "json") {
         $jsonArray[] = json_decode( file_get_contents($rulesetdir . "/" . $name), true);
     } else if (explode(".", $name)[1] == "php"){
--- ruleset/f1176cc3-9312-4024-8789-5d7c4bf28798.php
+++ ruleset/f1176cc3-9312-4024-8789-5d7c4bf28798.php
@@ -3,5 +3,5 @@ $json = array("id"=> "f1176cc3-9312-4024-8789-5d7c4bf28798", "enabled"=> true, "
   "rules"=> [array("action"=> "PROXY", "pattern"=> "CN", "type"=> "GEOIP", "order"=> "0"), array("action"=> "PROXY", "pattern"=> ".cn", "type"=> "URL", "order"=> "0")],
   "created_at"=> "2016-06-14T14:46:32", "is_official"=> true, "name"=> "中国地区走代理", "description"=> "用于国外翻回中国");
 
-if (strpos($_SERVER['REQUEST_URI'], $json["id"]) !== FALSE)
+if (!isset($_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], $json["id"]) !== FALSE)
     echo json_encode( $json);
