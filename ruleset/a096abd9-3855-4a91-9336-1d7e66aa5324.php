ee8d2e79190eb0891dcd7ed6621f8d01755a88cd
liruqi <liruqi@gmail.com>
2016-10-14 19:15:35 +0800

    Update buildin rules

--- ruleset/a096abd9-3855-4a91-9336-1d7e66aa5324.php
+++ ruleset/a096abd9-3855-4a91-9336-1d7e66aa5324.php
@@ -13,6 +13,7 @@ if($REJECT) {
     }
     fclose($REJECT);
 }
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'bilibili.com', 'type'=>'DOMAIN-SUFFIX', 'order' => '0');
 $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'CN', 'type'=>'GEOIP', 'order' => '0');
 $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.cn', 'type'=>'URL', 'order' => '0');
 $json['name'] = '中国地区广告屏蔽后直连';
