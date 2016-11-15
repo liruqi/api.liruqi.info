7e41c7f1fa7e440c8aa3a34d3262bb6dd3352f6e
liruqi <liruqi@gmail.com>
2016-11-15 14:44:22 +0800

    No proxy CN rule for Chinese

--- mume-rulesets.php
+++ mume-rulesets.php
@@ -1,6 +1,9 @@
 <?php
 $names = array();
 $names['f1176cc3-9312-4024-8789-5d7c4bf28797'] = 1;
+if ($_GET['lang'] == 'zh-Hans-CN' || $_GET['lang'] == 'en-CN') {
+    $names['f1176cc3-9312-4024-8789-5d7c4bf28798'] = 1;
+}
 $rulesetdir =  dirname(__FILE__) . "/ruleset";
 $files = array_diff(scandir($rulesetdir), ['.', '..']);
 
