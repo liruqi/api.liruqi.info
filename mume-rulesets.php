bc83d5e3ed18643e13fa9b712d689bf6722efd8a
liruqi <liruqi@gmail.com>
2017-07-30 14:04:11 +0800

    Fix mume rules

--- mume-rulesets.php
+++ mume-rulesets.php
@@ -1,13 +1,22 @@
 <?php
-require 'vendor/autoload.php';
-use GeoIp2\Database\Reader;
+
+$jsonArray = [];
+$langs = explode('-', $_GET['lang']);
+$country = end($langs); 
+if (strlen($country) !== 2) {
+    echo json_encode($jsonArray);
+    exit();
+}
+
+// require 'vendor/autoload.php';
+// use GeoIp2\Database\Reader;
 
 $names = array();
 $names['f1176cc3-9312-4024-8789-5d7c4bf28797'] = 1;
 
-$mmdbReader = new Reader('/srv/http/106.187.88.85/vpn/GeoLite2-City_20170606/GeoLite2-City.mmdb');
-$record = $mmdbReader->city($_SERVER['REMOTE_ADDR']);
-if (substr($_GET['lang'],0,2)=='zh' || $record->country->isoCode === 'CN') {
+// $mmdbReader = new Reader('/srv/http/106.187.88.85/vpn/GeoLite2-City_20170606/GeoLite2-City.mmdb');
+// $record = $mmdbReader->city($_SERVER['REMOTE_ADDR']);
+if ($country === 'CN') {
     $names['f1176cc3-9312-4024-8789-5d7c4bf28798'] = 1;
 } else {
     $names['833fb590-f3d2-419f-9b64-e4879f2ed5a3'] = 1; //proxy rules
@@ -16,7 +25,6 @@ if (substr($_GET['lang'],0,2)=='zh' || $record->country->isoCode === 'CN') {
 $rulesetdir =  dirname(__FILE__) . "/ruleset";
 $files = array_diff(scandir($rulesetdir), ['.', '..']);
 
-$jsonArray = [];
 foreach($files as $key => $name) {
     $uuid = explode(".", $name)[0];
     if (isset($names[$uuid])) continue;
