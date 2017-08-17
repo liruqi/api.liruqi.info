0c81631275a682c471fdb144299e671e00448efe
liruqi <liruqi@gmail.com>
2017-08-17 13:01:35 +0800

    Update rules data

--- ruleset/a096abd9-3855-4a91-9336-1d7e66aa5323.php
+++ ruleset/a096abd9-3855-4a91-9336-1d7e66aa5323.php
@@ -1,22 +1,24 @@
 <?php
 
-if (($_GET['version']==='1.7.5' || ($_GET['version'] === '1.3') || $_GET['api'] == 2) && $country==='CN') {
     $basedir = dirname(dirname(__FILE__)) . "/";
     $jsonstr = file_get_contents($basedir . "ruleset/a096abd9-3855-4a91-9336-1d7e66aa5323.mume");
     $json = json_decode($jsonstr, true);
     $json['id'] = 'a096abd9-3855-4a91-9336-1d7e66aa5323';
-    $REJECT = fopen($basedir . "data/lhie1_cn_ads.txt","r");
+    $REJECT = fopen($basedir . "data/h2y_ads.txt","r");
     if($REJECT) {
         while(!feof($REJECT)) {
             $line = trim(fgets($REJECT));
             $arr = explode(",", $line);
-            if (count($arr) != 2) break;
-            $json["rules"][] = array('action'=>'REJECT', 'pattern'=>$arr[1], 'type'=>$arr[0], 'order' => '0');
+            if (count($arr) != 1) break;
+            $json["rules"][] = array('action'=>'REJECT', 'pattern'=>$arr[0], 'type'=>'DOMAIN-SUFFIX', 'order' => '0');
         }
         fclose($REJECT);
     }
 
-    $json['name'] = '阻止无用的网络请求';
+if ($country==='CN') {
+    $json['name'] = '阻止广告类的网络请求';
+} else {
+    $json['name'] = 'Block Ads traffic';
+}
     if (!isset($_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], $json["id"]) !== FALSE) 
         echo json_encode($json, JSON_PRETTY_PRINT);
-}
