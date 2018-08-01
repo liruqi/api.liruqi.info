b7f6bad2a659f269feb4a6594772de520c97e911
root <liruqi@gmail.com>
2018-08-01 05:52:06 +0000

    Fix baidu rule

--- ruleset/a096abd9-3855-4a91-9336-1d7e66aa5322.php
+++ ruleset/a096abd9-3855-4a91-9336-1d7e66aa5322.php
@@ -2,35 +2,37 @@
 $basedir = dirname(dirname(__FILE__));
 $json = array("is_official" => True, "description"=>"避免影响国内站点访问速度");
 $json['id'] = 'a096abd9-3855-4a91-9336-1d7e66aa5322';
-/*
-$DIRECT = fopen($basedir . "/data/DIRECT","r");
-if($DIRECT) {
-    while(!feof($DIRECT)) {
-        $line = trim(fgets($DIRECT));
-        $arr = explode(",", $line);
+$od = "0";
+
+$GFW = fopen(dirname($basedir) . "/mume.red/etc/gfw.m16","r");
+if($GFW) {
+    while(!feof($GFW)) {
+        $line = trim(fgets($GFW));
+        $arr = explode(".", $line);
         if (count($arr) != 2) break;
-        $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>$arr[1], 'type'=>$arr[0], 'order' => '0');
+        $json["rules"][] = array('action'=>'PROXY', 'pattern'=>$line . '.0.0/16', 'type'=>'IP-CIDR', 'order' => $od);
     }
-    fclose($DIRECT);
-}*/
+    fclose($GFW);
+}
+
+$od = "1";
 $DIRECT = fopen(dirname($basedir) . "/mume.red/etc/cn30.m8","r");
 if($DIRECT) {
     while(!feof($DIRECT)) {
         $line = trim(fgets($DIRECT));
         $arr = explode(".", $line);
         if (count($arr) != 2) break;
-        $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>$line . '0.0.0/8', 'type'=>'IP-CIDR', 'order' => '0');
+        $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>$line . '0.0.0/8', 'type'=>'IP-CIDR', 'order' => $od);
     }
     fclose($DIRECT);
 }
-if (intval($userIP) > 250) {
-    $json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'CN', 'type'=>'GEOIP', 'order' => '0');
-}
-$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.cn', 'type'=>'URL', 'order' => '1');
-$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.qq.com', 'type'=>'URL', 'order' => '1');
-$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'baidu.com', 'type'=>'URL', 'order' => '1');
-$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'captive.apple.com', 'type'=>'DOMAIN-SUFFIX', 'order' => '1');
-$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'icloud.com', 'type'=>'DOMAIN-SUFFIX', 'order' => '1');
+
+$od = "2";
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.cn', 'type'=>'URL', 'order' => '2');
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.qq.com', 'type'=>'URL', 'order' => '2');
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'.baidu.com', 'type'=>'URL', 'order' => '2');
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'captive.apple.com', 'type'=>'DOMAIN-SUFFIX', 'order' => '2');
+$json["rules"][] = array('action'=>'DIRECT', 'pattern'=>'icloud.com', 'type'=>'DOMAIN-SUFFIX', 'order' => '2');
 $json['name'] = '中国地区直连';
 
 if ($_GET['q'] != $json['id']) {
