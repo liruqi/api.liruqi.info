e9307f8d1b90dfcc41c881596e6e6ba189baed68
root <liruqi@gmail.com>
2020-12-01 09:42:45 +0000

    Add testflight app list

---
+++ index.php
@@ -0,0 +1,56 @@
+{
+    "version": 1,
+    "code": 0,
+    "offset": 6,
+    "data": [
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "Telegram",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeCom",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },{
+            "name": "网易云音乐",
+            "tf": "https://testflight.apple.com/join/https://testflight.apple.com/join/QcbhHINF",
+            "icon": "https://is5-ssl.mzstatic.com/image/thumb/Purple114/v4/3d/f0/54/3df054fe-138d-9a9c-3e66-5791ff630c22/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1920x1080bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        },
+        {
+            "name": "WeChat",
+            "tf": "https://testflight.apple.com/join/kcE1u2kR",
+            "icon": "https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/f5/f7/af/f5f7afc6-f30e-e80a-fa87-b43c77b0a9b9/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb-80.png"
+        }
+    ]
+}
